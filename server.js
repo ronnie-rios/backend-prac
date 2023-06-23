3088bd44537541085beb1b5ef37e410c5ce0b591
Ronnie Rios <ronniesmail14@gmail.com>
2023-06-23 09:02:29 -0500

    server

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const cors = require('cors');
 const app = express();
 
 const PORT = 9002;
+require("./config/mongoose.config");
+
+app.use(cors(), express.json(), express.urlencoded({ extended: true }));
+
+
+app.listen(PORT, () => console.log(`server is running on ${PORT}`))
