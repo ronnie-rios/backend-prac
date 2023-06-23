74555a5fc7884fb43f0ac2434b3bfe3b621f0195
Ronnie Rios <ronniesmail14@gmail.com>
2023-06-23 09:19:13 -0500

    middleware

--- server.js
+++ server.js
@@ -13,4 +13,21 @@ app.use(cors(), express.json(), express.urlencoded({ extended: true }));
 app.use('/api/users', userRoutes);
 app.use('/api/notes',noteRoutes)
 
+app.use((req, res) => {
+    res.status(404)
+        .json({
+            error: 'route not found'
+        })
+});
+
+app.use((err, req, res, next) => {
+    let errorStatus = err.status || 500;
+    let errorMessage = err.message || 'problem with your request';
+    res.json({
+        status: errorStatus,
+        message: errorMessage
+    });
+});
+
+
 app.listen(PORT, () => console.log(`server is running on ${PORT}`))
