dce88c3c26113383da135f7223754dad2ce184a9
Ronnie Rios <ronniesmail14@gmail.com>
2023-06-23 09:11:01 -0500

    user controller

---
+++ controller/todos.controller.js
@@ -0,0 +1,2 @@
+const User = require("../models/user.model");
+const Note = require("../models/notes.model");
---
+++ controller/user.controller.js
@@ -0,0 +1,36 @@
+const User = require("../models/user.model");
+
+const getAll = async (req, res) => {
+  try {
+    const users = await User.find();
+    return res.json(users);
+  } catch (error) {
+    res.status(400).json({ err: error });
+  }
+};
+
+const getOneUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).populate("notes");
+    return user
+      ? res.json(user)
+      : res.status(404).json({ message: "User not found" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    return res.json({  user: user });
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+};
+
+module.exports = {
+  getAll,
+  getOneUser,
+  createUser
+};
