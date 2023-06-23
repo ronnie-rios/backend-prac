fb4a787247343cf24ece276d0e8f737ed6e59677
Ronnie Rios <ronniesmail14@gmail.com>
2023-06-23 09:12:17 -0500

    todo

--- controller/todos.controller.js
+++ controller/todos.controller.js
@@ -1,2 +1,23 @@
 const User = require("../models/user.model");
-const Note = require("../models/notes.model");
+const Todo = require("../models/todos.model");
+
+const postNote = async (req, res) => {
+    try {
+      const newTodo = await Todo.create(req.body);
+    
+      const foundUser = await User.findOneAndUpdate(
+        { _id: req.params.id },
+        { $push: { todos: newTodo._id } },
+        { new: true }
+      );
+     
+      res.json(foundUser);
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  };
+  
+  
+  module.exports = {
+      postNote
+  }
