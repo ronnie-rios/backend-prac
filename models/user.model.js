9b64b409e6e8c9587b52ec95c4cea8bbd027617a
Ronnie Rios <ronniesmail14@gmail.com>
2023-06-23 09:06:56 -0500

    rename files cause wrong spot

---
+++ models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+        type: String
+    },
+
+    todos: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Todo'
+    }]
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
