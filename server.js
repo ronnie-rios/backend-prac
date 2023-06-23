231e3af318884f1d46a5f534cad2f53e8181756b
Ronnie Rios <ronniesmail14@gmail.com>
2023-06-23 09:17:32 -0500

    added routes to server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { userRoutes } = require('./routes/user.routes');
+const { noteRoutes } = require('./routes/todos.routes');
 
 const app = express();
 
@@ -8,5 +10,7 @@ require("./config/mongoose.config");
 
 app.use(cors(), express.json(), express.urlencoded({ extended: true }));
 
+app.use('/api/users', userRoutes);
+app.use('/api/notes',noteRoutes)
 
 app.listen(PORT, () => console.log(`server is running on ${PORT}`))
